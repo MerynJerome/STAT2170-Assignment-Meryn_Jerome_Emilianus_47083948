acaec3614596942e0da2848bae0cb4d1686d9670
manuelpillai jerome emilianus <AaranaJerome@Meryn-MacBook-Air.local>
2022-05-19 17:26:51 +1000

    Second Commit

--- STAT2170-Meryn_Jerome_Emilianus_47083948.R
+++ STAT2170-Meryn_Jerome_Emilianus_47083948.R
@@ -16,7 +16,6 @@ boxplot(Score ~ as.factor(Gender) + as.factor(Genre), data = movie)
 movie.lm = lm(Score ~ as.factor(Gender) + as.factor(Genre), data = movie)
 anova(movie.lm)
 summary(movie.lm)$coefficients
-
 # Yi = µ + αi + βj + γij + εi
 # Yi = score response
 # µ = overall mean
